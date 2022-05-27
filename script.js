deae0a15bf386ee7d2ee5b2aaa148fb914383cfd
abirraiyan <106406133+abirraiyan@users.noreply.github.com>
2022-05-28 01:44:53 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
+
+
+const newYear = "21 sep 2022";
+
+
+function countdown() {
+    const newYearDate = new Date(newYear);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) %60;
+   const seconds = Math.floor(totalSeconds) %60;
+
+   console.log(days , hours , mins , seconds)
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formateTime(hours);
+    minsEl.innerHTML = formateTime(mins);
+    secondsEl.innerHTML = formateTime(seconds);
+
+}
+function formateTime(time){
+
+    return time < 10 ? `0${time}` : time;
+}
+
+
+//initial call
+
+countdown();
+setInterval(countdown, 1000);
